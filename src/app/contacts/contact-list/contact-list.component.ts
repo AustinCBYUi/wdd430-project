af3a6277b642f680ff686b66740855657ce107de
Austin Campbell <131830841+AustinCBYUi@users.noreply.github.com>
2024-09-28 09:37:06 -0600

    Finishing week 2, finished contact-list component and app-component fixes.

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -22,7 +22,6 @@ export class ContactListComponent implements OnInit {
   contact: Contacts[] = [
     new Contacts('1', 'Austin Campbell', 'cam23021@byui.edu', '6022955366',
       'https://img.freepik.com/premium-vector/default-image-icon-vector-missing-picture-page-website-design-mobile-app-no-photo-available_87543-11093.jpg'),
-    new Contacts('2', "Jessi Campbell", 'campbelljessib@gmail.com', '4804907712', 'https://img.freepik.com/premium-vector/default-image-icon-vector-missing-picture-page-website-design-mobile-app-no-photo-available_87543-11093.jpg'),
     new Contacts('3', 'Bro. Jackson', 'brotherjackson@gmail.com', '1343199', '/Images/jacksonk.jpg'),
     new Contacts('4', 'Bro. Barzee', 'brotherbarzee@gmail.com', '95149591', '/Images/barzeer.jpg')
   ];
--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {ContactDetailComponent} from "./contact-detail/contact-detail.component";
+import {ContactListComponent} from "./contact-list/contact-list.component";
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [],
+  imports: [
+    ContactDetailComponent,
+    ContactListComponent
+  ],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.css'
 })
